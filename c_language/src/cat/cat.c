52b8635e031f8184f752b150c58e634dcb7956e4
Lynsey Pinsir <renal98@mail.ru>
2023-09-15 17:01:37 +0300

    +cat

--- c_language/src/cat/cat.c
+++ c_language/src/cat/cat.c
@@ -1,51 +1,27 @@
-#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <unistd.h>
 
-struct all_flags {
-  int flag_b;
-  int flag_e;
-  int flag_n;
-  int flag_s;
-  int flag_t;
-  int flag_v;
-  int flag_fail;
-};
-
-struct all_flags check_flags(int argc, char **argv);
-void open_file(int argc, char **argv, struct all_flags value);
-void read_file(int *flag_for_start_num, int *numeration, struct all_flags value,
-               FILE *files);
-void all_print(int *flag_for_start_num, int *numeration, struct all_flags value,
-               char c, char *c2);
-void print_numeration(int *numeration);
+#include "cat.h"
 
 int main(int argc, char *argv[]) {
-  struct all_flags value;
-  value = check_flags(argc, argv);
+  struct CatFlags value = CatUtilityCheckFlags(argc, argv);
+
   if (value.flag_fail == 1) {
-    fprintf(stderr, "Error: comman no found");
+    fprintf(stderr, "Error: command no found");
   } else {
-    if (value.flag_b == 1 && value.flag_n == 1) {
-      value.flag_n = 0;
-    }
-    open_file(argc, argv, value);
+    CatUtilityOpenFile(argc, argv, &value);
   }
+
   return 0;
 }
 
-struct all_flags check_flags(int argc, char **argv) {
-  struct all_flags num = {0};
-  struct option long_flags[] = {{"number-nonblank", no_argument, NULL, 'b'},
-                                {"squeeze-blank", no_argument, NULL, 's'},
-                                {"number", no_argument, NULL, 'n'},
-                                {NULL, 0, NULL, 0}};
-  int rez = 0;
-  int option_index;
-  while ((rez = getopt_long(argc, argv, "+beEnsvtT", long_flags,
-                            &option_index)) != -1) {
-    switch (rez) {
+struct CatFlags CatUtilityCheckFlags(int argc, char **argv) {
+  struct CatFlags num = {0};
+  int result = 0;
+  while ((result = CatUtilityUsingGetOpt(argc, argv)) != -1) {
+    switch (result) {
       case 'b':
         num.flag_b = 1;
         break;
@@ -77,82 +53,122 @@ struct all_flags check_flags(int argc, char **argv) {
         break;
     }
   }
+  if (num.flag_b == 1 && num.flag_n == 1) {
+      num.flag_n = 0;
+  }
   return num;
 }
 
-void open_file(int argc, char **argv, struct all_flags value) {
+int CatUtilityUsingGetOpt(int argc, char **argv) {
+  int option_index;
+  struct option long_flags[] = {
+      {"number-nonblank", no_argument, NULL, 'b'},
+      {"squeeze-blank", no_argument, NULL, 's'},
+      {"number", no_argument, NULL, 'n'},
+      {NULL, 0, NULL, 0}};
+    (void)long_flags;
+  return getopt_long(argc, argv, "+beEnsvtT", long_flags, &option_index);
+}
+
+void CatUtilityOpenFile(int argc, char **argv, struct CatFlags *value) {
   FILE *files;
   for (int i = 1; i < argc; i++) {
-    int flag_for_start_num = 0;
-    int numeration = 1;
-    if (argv[i][0] != '-') {
-      if ((files = fopen(argv[i], "r")) == NULL) {
-        fprintf(stderr, "%s: no such file\n", argv[i]);
-      } else {
-        read_file(&flag_for_start_num, &numeration, value, files);
-      }
+    if (argv[i][0] == '-') {  // skip the flags
+        continue;
+    }
+    if ((files = fopen(argv[i], "r")) == NULL) {
+       fprintf(stderr, "%s: no such file\n", argv[i]);
+    } else {
+       CatUtilityReadFile(value, files);
     }
   }
   fclose(files);
 }
 
-void read_file(int *flag_for_start_num, int *numeration, struct all_flags value,
-               FILE *files) {
-  char c;
-  char c2[3] = {' ', ' ', '\n'};
-  while ((c = getc(files)) != EOF) {
-    c2[0] = c2[1];
-    c2[1] = c2[2];
-    c2[2] = c;
-    if ((c2[0] != '\n' || c2[1] != '\n') ||
-        (c2[2] != '\n' || value.flag_s != 1)) {
-      all_print(flag_for_start_num, numeration, value, c, c2);
+void CatUtilityReadFile(struct CatFlags *value, FILE *files) {
+  struct CatReadFile data;
+  data.last_symvols[0] = ' ';
+  data.last_symvols[1] = ' ';
+  data.last_symvols[2] = '\n';
+  data.flag_n = 0;
+  data.num = 1;
+  
+  while ((data.symvol = getc(files)) != EOF) {
+    data.last_symvols[0] = data.last_symvols[1];
+    data.last_symvols[1] = data.last_symvols[2];
+    data.last_symvols[2] = data.symvol;
+    if ((data.last_symvols[0] != '\n' || data.last_symvols[1] != '\n') ||
+        (data.last_symvols[2] != '\n' || value -> flag_s != 1)) {
+      CatUtilityAllPrint(value, &data);
     }
   }
 }
 
-void all_print(int *flag_for_start_num, int *numeration, struct all_flags value,
-               char c, char *c2) {
-  int flag = 0;
-  if (*flag_for_start_num == 0) {
-    if (value.flag_b == 1) {
-      if (c2[2] != '\n' && c2[1] == '\n') {
-        flag = 1;
-      }
-    } else if (value.flag_n == 1) {
-      flag = 1;
+
+
+void CatUtilityAllPrint(struct CatFlags *value, struct CatReadFile *data) {
+  int mode = 0;
+  if (data -> flag_n == 0) {
+    mode = CatUtilityModeForNumeration(value, data);
+  }
+  if (mode == 1) {
+    CatUtilityPrintNumeration(data);
+  }
+  CatUtilityModeForNewLine(value, data);
+  if (CatUtilityModeForTabs(value, data) == 2) {
+    mode = 2;
+  } 
+  if (value -> flag_v == 1 && (data -> symvol != '\t' && data -> symvol != '\n')) {
+    CatUtilityPrintSymbol(data, &mode);
+  }
+  if (mode != 2) {
+    printf("%c", data -> symvol);
+  }
+}
+
+int CatUtilityModeForNumeration(struct CatFlags *value, struct CatReadFile *data) {
+  int mode = 0;
+  if (value -> flag_b == 1) {
+    if (data -> last_symvols[2] != '\n' && data -> last_symvols[1] == '\n') {
+      mode = 1;
     }
+  } else if (value -> flag_n == 1) {
+   mode = 1;
   }
-  if (flag == 1) {
-    print_numeration(numeration);
-  }
-  if (c == '\n') {
-    *flag_for_start_num = 0;
-    if (value.flag_e == 1) {
+  return mode;
+}
+
+void CatUtilityPrintNumeration(struct CatReadFile *data) {
+  printf("%6d\t", data -> num);
+  data -> num += 1;
+}
+
+void CatUtilityModeForNewLine(struct CatFlags *value, struct CatReadFile *data) {
+  if (data -> symvol == '\n') {
+    data -> flag_n = 0;
+    if (value -> flag_e == 1) {
       printf("$");
     }
   } else {
-    *flag_for_start_num = 1;
-  }
-  if (c == '\t' && (value.flag_t == 1)) {
-    printf("^I");
-    flag = 2;
-  }
-  if (value.flag_v == 1 && (c != '\t' && c != '\n')) {
-    if ((c < 32 && c != 9) && c != 10) {
-      printf("^%c", c + 64);
-      flag = 2;
-    } else if (c == 127) {
-      printf("^%c", c - 64);
-      flag = 2;
-    }
-  }
-  if (flag != 2) {
-    printf("%c", c);
+    data -> flag_n = 1;
   }
 }
 
-void print_numeration(int *numeration) {
-  printf("%6d\t", *numeration);
-  *numeration += 1;
+int CatUtilityModeForTabs(struct CatFlags *value, struct CatReadFile *data) {
+  int mode = 0;
+  if (data -> symvol == '\t' && (value -> flag_t == 1)) {
+    printf("^I");
+    mode = 2;
+  }
+  return mode;
+}
+
+void CatUtilityPrintSymbol(struct CatReadFile *data, int *mode) {
+    if ((data -> symvol < 32 && data -> symvol != 9) && data -> symvol!= 10) {
+      printf("^%c", data -> symvol + 64);
+      *mode = 2;
+    } else if (data -> symvol == 127) {
+      printf("^%c", data -> symvol - 64);
+      *mode = 2;
+    }
 }
--- c_language/src/cat/cat.h
+++ c_language/src/cat/cat.h
@@ -1,1 +1,34 @@
+#ifndef UTILITES_C_LANGUAGE_CAT_CAT_H_
+#define UTILITES_C_LANGUAGE_CAT_CAT_H_
 
+struct CatFlags {
+  int flag_b;
+  int flag_e;
+  int flag_n;
+  int flag_s;
+  int flag_t;
+  int flag_v;
+  int flag_fail;
+};
+
+struct CatReadFile {
+  int flag_n; // flag_for_start_num
+  int num; // numeration
+  char symvol;
+  char last_symvols[];
+};
+
+struct CatFlags CatUtilityCheckFlags(int argc, char **argv);
+int CatUtilityUsingGetOpt(int argc, char **argv);
+void CatUtilityOpenFile(int argc, char **argv, struct CatFlags *value);
+void CatUtilityReadFile(struct CatFlags *value, FILE *files);
+
+void CatUtilityAllPrint(struct CatFlags *value, struct CatReadFile *data);
+void CatUtilityModeForNewLine(struct CatFlags *value, struct CatReadFile *data);
+void CatUtilityPrintSymbol(struct CatReadFile *data, int *mode);
+void CatUtilityPrintNumeration(struct CatReadFile *data);
+int CatUtilityModeForTabs(struct CatFlags *value, struct CatReadFile *data);
+int CatUtilityModeForNumeration(struct CatFlags *value, struct CatReadFile *data);
+
+
+#endif // UTILITES_C_LANGUAGE_CAT_CAT_H_
