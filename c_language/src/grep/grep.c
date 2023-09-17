7bbe82e44b50f7ce5413de74a1a531c22239677e
Lynsey Pinsir <lynseypi-god_of_programming>
2023-09-17 20:52:57 +0300

    process updating grap

--- c_language/src/grep/grep.c
+++ c_language/src/grep/grep.c
@@ -4,19 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct all_flags {
-  int e;
-  int i;
-  int v;
-  int c;
-  int l;
-  int n;
-  int h;
-  int s;
-  int f;
-  int o;
-  int fail;
-};
 
 void check_flags(int argc, char **argv);
 char *realization_flag_f(int *size_p, char **pattern, char *optarg,
@@ -34,190 +21,256 @@ void printf_in_loop(struct all_flags value, char *string_file, char *string,
                     int n, int *sf);
 
 int main(int argc, char **argv) {
-  char *check_memmory = NULL;
-  check_memmory = (char *)malloc(sizeof(char));
-  if (check_memmory == NULL) {
-    fprintf(stderr, "Memmory is not allocated");
+  if (argc > 2) {
+    GrepUtilityCheckFlags(argc, argv);
   } else {
-    free(check_memmory);
-    if (argc > 2) {
-      check_flags(argc, argv);
-    } else {
-      fprintf(stderr, "No such command");
-    }
+    fprintf(stderr, "No such command");
   }
 
   return 0;
 }
 
-void check_flags(int argc, char **argv) {
-  char *pattern = NULL;
-  pattern = (char *)malloc(sizeof(char));
-  struct all_flags value = {0};
-  int last_position = 1;
-  int rezult = 0;
-  int size_p = 0;
-  while ((rezult = getopt_long(argc, argv, "e:ivclnhsf:o", NULL, NULL)) != -1) {
-    switch (rezult) {
-      case 'e': {
-        if (size_p == 0) {  // grep -e -i pattern text.tc  value.e == 0
-          value.e = 1;
-          int z = strlen(optarg);
-          size_p = z + 1;
-          pattern = (char *)realloc(pattern, size_p * sizeof(char));
-          strcpy(pattern, optarg);
-        } else {
-          int z = strlen(optarg);
-          char c[2] = "|";
-          size_p += z + 3;
-          pattern = (char *)realloc(pattern, size_p * sizeof(char));
-          strcat(pattern, c);
-          strcat(pattern, optarg);
-        }
-        break;
-      }
-      case 'i':
-        value.i = 1;
-        break;
-      case 'v':
-        value.v = 1;
-        break;
-      case 'c':
-        value.c = 1;
-        break;
-      case 'l':
-        value.l = 1;
-        break;
-      case 'n':
-        value.n = 1;
-        break;
-      case 'h':
-        value.h = 1;
-        break;
-      case 's':
-        value.s = 1;
-        break;
-      case 'f':
-        value.f = 1;
-        pattern = realization_flag_f(&size_p, &pattern, optarg, &value);
-        break;
-      case 'o':
-        value.o = 1;
-        break;
-      default:
-        value.fail = 1;
-        break;
-    }
+// Memory allocation One-dimensional array
+int GrepUtilityMemoryAllocTDArray(char **str, size_t *size_str, size_t need_size) {
+  int fail = 0;
+  *str = NULL;
+  *str = (char *)malloc(need_size * sizeof(char));
+  *size_str = need_size;
+  if (*str == NULL) {
+    fprintf(stderr, "Memory was not allocated");
+    fail = 1;
   }
-  if (value.fail == 0) {
-    if (value.e == 0 && value.f == 0) {
-      new_pattern(argc, argv, &pattern, &last_position, value);
-    } else {
-      massive_files(argc, argv, pattern, &last_position, value);
-    }
-  } else {
-    fprintf(stderr, "no such commands");
-  }
-  free(pattern);
+
+  return fail;
 }
 
-char *realization_flag_f(int *size_p, char **pattern, char *optarg,
-                         struct all_flags *value) {
-  FILE *f;
-  if ((f = fopen(optarg, "r")) == NULL) {
+// Memory Overexpression One-dimensional array
+int GrepUtilityMemoryOverODArray(char **str, size_t *size_str, size_t need_size) {
+  int fail = 0;
+  char *new_str;
+  fail = GrepMemoryAllocation(new_str, size_str, need_size);
+  if (fail == 0) {
+    memcpy(new_str, *str, *size_str);
+    free(*str);
+    *str = new_str;
+    *size_str = need_size;
+  }
+
+  return fail;
+}
+
+
+void GrepUtilityCheckFlags(int argc, char **argv) {
+  struct GrepDates data;
+  data.pattern.size_p = 1;
+  data.fail = 0;
+  if (GrepUtilityMemoryAllocTDArray(&data.pattern.str_p, &data.pattern.size_p, 1)) {
+    int res = 0;
+    while ((res = getopt_long(argc, argv, "e:ivclnhsf:o", NULL, NULL)) != -1 && data.fail == 0) {
+      switch (res) {
+        case 'f': GrepUtilityRealizationFlagF(&data); break;
+        case 'e': GrepUtilityRealizationFlagE(&data); break;
+        case 'i': data.mode.i = 1; break;
+        case 'v': data.mode.v = 1; break;
+        case 'c': data.mode.c = 1; break; 
+        case 'l': data.mode.l = 1; break;
+        case 'n': data.mode.n = 1; break;
+        case 'h': data.mode.h = 1; break;
+        case 's': data.mode.s = 1; break;
+        case 'o': data.mode.o = 1; break;
+        default: data.fail = 2; break;
+      }
+    }
+    GrepUtilityRealization(argc, argv, data);
+    free(data.pattern.str_p);
+  }
+}
+
+// opening a file to take a pattern
+void GrepUtilityOpenFileInFlagF(struct GrepDates *data) {
+  FILE *file_stream;
+  file_stream = NULL;
+  file_stream = fopen(optarg, "r");
+  if (file_stream == NULL) {
     fprintf(stderr, "%s: no such file\n", optarg);
-    value->fail = 1;
+    data->fail = 1
   } else {
-    char *string = NULL;
-    size_t size_string = 0;
-    while (getline(&string, &size_string, f) != -1) {
-      if (string[strlen(string) - 1] == '\n' && strlen(string) > 2) {
-        string[strlen(string) - 1] = '\0';
-      }
-      if (*size_p != 0) {
-        char c[2] = "|";
-        *size_p += size_string + 5;
-        *pattern = (char *)realloc(*pattern, *size_p * sizeof(char));
-        strcat(*pattern, c);
-        strcat(*pattern, string);
-      } else {
-        *size_p += size_string + 2;
-        *pattern = (char *)realloc(*pattern, *size_p * sizeof(char));
-        strcpy(*pattern, string);
-      }
-      *size_p += size_string + 2;
-    }
-    free(string);
+    GrepUtilityRealizationInFlagF(file_stream, data);
+    fclose(file_stream);
   }
-  fclose(f);
-  return *pattern;
+  data -> mode.f = 1;
 }
 
-void new_pattern(int argc, char **argv, char **pattern, int *lp,
-                 struct all_flags value) {
-  for (int i = 1; i < argc; i++) {
+// Take pattern
+void GrepUtilityRealizationInFlagF(FILE* file_stream, struct GrepDates *data) {
+  char *str = NULL;
+  size_t size_str = 0;
+  while (getline(&str, &size_str, file_stream) != -1) {
+    if (str[strlen(str) - 1] == '\n' && strlen(str) > 2) {
+        str[strlen(str) - 1] = '\0';
+    }
+    size_t mode = (data -> pattern.size_p == 1) ? 2 : 5;
+    size_t need_size = size_str + mode;
+    if (GrepUtilityMemoryOverODArray(&data ->pattern.str_p, &data -> pattern.size_p, need_size)) {
+      if (mode == 1) {
+        strcpy(data ->pattern.str_p, str);
+      } else {
+        char sepparator[] = "|";
+        strcat(data ->pattern.str_p, sepparator);
+        strcat(data ->pattern.str_p, str);
+      }
+    }
+  }
+  if (str != NULL) {
+    free(str)
+  }
+}
+
+// Take pattern
+void GrepUtilityRealizationFlagE(struct GrepDates *data) {
+  data -> mode.e = 1;
+  size_t mode = (data -> pattern.size_p == 1) ? 1 : 3;
+  size_t need_size = strlen(optarg) + mode;
+  if (GrepUtilityMemoryOverODArray(&data ->pattern.str_p, &data ->pattern.size_p, need_size)) {
+    if (mode == 1) {
+      strcpy(data ->pattern.str_p, optarg);
+    } else {
+      char sepparator[] = "|";
+      strcat(data ->pattern.str_p, sepparator);
+      strcat(data ->pattern.str_p, optarg);
+    }
+  } else {
+    data -> fail = 1;
+  }
+}
+
+void GrepUtilityRealization(int argc, char **argv, struct GrepDates *data) {
+  if (data -> fail == 2) {
+    fprintf(stderr, "no such commands");
+  } else {
+    if (data -> flags.size == 1) { // there were no <e> and <f> flags
+      GrepUtilityTakingAPattern(argc, argv, data);
+    } 
+    if (data -> fail == 0) {
+      GrepUtilityArrayFiles(argc, argv, data);
+    }
+  }
+}
+
+// Take pattern
+void GrepUtilityTakingAPattern(int argc, char **argv, struct GrepDates *data) {
+  size_t need_size = 0;
+  for (size_t i = 1; i < argc && need_size == 0, ++i) {
     if (argv[i][0] != '-') {
-      int size_p = strlen(*argv) + 1;
-      *pattern = (char *)realloc(*pattern, size_p * sizeof(char));
-      strcpy(*pattern, argv[i]);
-      *lp = i + 1;
-      break;
+      size_t new_size = strlen(*argv) + 1;
+      if (GrepUtilityMemoryOverODArray(&data ->inp.pattern, &data -> inp.size, need_size)) {
+        strcpy(*pattern, argv[i]);
+        data -> last_pisition = i + 1;
+      }
     }
   }
-  massive_files(argc, argv, *pattern, lp, value);
 }
 
-void massive_files(int argc, char **argv, char *pattern, int *lp,
-                   struct all_flags value) {
-  char **files = NULL;
-  int size_file = 0;
-  files = (char **)malloc(sizeof(char *));
-  for (int i = *lp; i < argc; i++) {
-    if (strcmp("-e", argv[i]) == 0 || strcmp("-f", argv[i]) == 0) {
-      i++;
-    } else if (argv[i][0] != '-') {
-      size_file++;
-      files = (char **)realloc(files, (size_file) * sizeof(char *));
-      files[size_file - 1] =
-          (char *)malloc((strlen(argv[i]) + 1) * sizeof(char));
-      if (files == NULL || files[size_file - 1] == NULL) {
-        fprintf(stderr, "memory is over =(");
-        size_file = -1;
-        break;
+// Memory allocation to a two-dimensional files array
+int GrepUtilityMemoryAllocTDFArray(char ***str, size_t *size_str, size_t need_size) {
+  *str = NULL;
+  *str = (char **)malloc(need_size, sizeof(char *));
+  *size_str = need_size;
+  int fail = 0;
+  if (*str == NULL) {
+    fprintf(stderr, "Memory was not allocated");
+    fail = 1;
+  }
+  return fail;
+}
+
+/* Memory Overexpression to a two-dimensional files array
+ * need_fs - we need to change the main size to this one (need first size)
+ * need_ss - we need to change the size of the derived matrix to this (need second size)
+ */
+int GrepUtilityMemoryOverTDFArray(char ***str, size_t *size_str ,size_t need_fs, size_t need_ss) {
+  int fail = 0;
+  char **new_str = NULL;
+  size_t new_size = 0;
+  if (GrepUtilityMemoryAllocTDFArray(&new_str, &new_size, need_fs)) {
+    for (size_t i = 0; i < *size_str; ++i) {
+      memcpy(new_str[i], (*str)[i], strlen((*str)[i]));
+    }
+    GrepUtilityCleanTDFArray(str, size_str);
+    *str = new_str;
+    *size_str = new_size;
+    size_t second_size = 0;
+    fail = GrepUtilityMemoryAllocTDArray(&(*str)[*size_str - 1], &second_size, need_ss);
+  } else {
+    fail = 1;
+  }
+  return fail;
+}
+
+void GrepUtilityCleanTDFArray(char ***str, size_t *size_str) {
+  for (size_t i = 0; i < *size_str; ++i) {
+    free(*str[i]);
+  }
+  free(*str);
+}
+
+// getting an array of files
+void GrepUtilityArrayFiles(int argc, char **argv, struct GrepDates *data) {
+  if (GrepUtilityMemoryAllocTDFArray(&data -> files.str_f, &data -> files.size_f, 1)) {
+    for (size_t i = data -> last_position; i < argc && data -> fail == 0; ++i) {
+      if (strcmp("-e", argv[i]) == 0 || strcmp("-f", argv[i]) == 0) {
+        ++i;
+      } else if (argv[i][0] != '-') {
+        data -> fail = GrepUtilityMemoryOverTDFArray(&data -> files.str_f, &data -> files.size_f, data -> files.size_f + 1, strlen(argv[i]) + 1);
+        strcpy(data -> files.str_f[data -> files.size_f - 1], argv[i]);
+      } else if (argv[i][strlen(argv[i]) - 1] == 'e' ||
+                 argv[i][strlen(argv[i]) - 1] == 'f') {
+        ++i;
+      }
+    }
+    if (data -> fail == 0) {
+      if (data -> files.size_f == 1) {
+        fprintf(stderr, "no such file or directory\n");
       } else {
-        strcpy(files[size_file - 1], argv[i]);
+        GrepUtilityOpenFile(data);
       }
-    } else if (argv[i][strlen(argv[i]) - 1] == 'e' ||
-               argv[i][strlen(argv[i]) - 1] == 'f') {
-      i++;
     }
+    GrepUtilityCleanTDFArray(data -> files.str_f, data -> files.size_f);
   }
-  if (size_file != -1) {
-    if (size_file == 0) {
-      fprintf(stderr, "no such file or directory\n");
-    } else {
-      open_file(files, &size_file, pattern, value);
-    }
-  }
-  for (int i = 0; i < size_file; i++) {
-    free(files[i]);
-  }
-  free(files);
 }
 
-void open_file(char **files, int *sf, char *pattern, struct all_flags value) {
-  FILE *f;
-  for (int t = 0; t < *sf; t++) {
-    if ((f = fopen(files[t], "r")) == NULL) {
-      if (value.s == 0) {
-        fprintf(stderr, "%s: no such file\n", files[t]);
+void GrepUtilityOpenFile(struct GrepDates *data) {
+  FILE *file_stream = NULL;
+  for (size_t i = 0; i < data -> files.size_f; ++i) {
+    if ((file_stream = fopen(data -> files.str_f[i], "r")) == NULL) {
+      if (data -> mode.s == 0) {
+        fprintf(stderr, "%s: no such file\n", data -> files.str_f[i]);
       }
     } else {
-      char *string_file = files[t];
-      read_file(string_file, sf, pattern, value, f);  // string = files[t]
-      fclose(f);
+      GrepUtilityReadFile(data, i);
+      fclose(file_stream);
     }
   }
+}
+
+void GrepUtilityReadFile(struct GrepDates *data, int *i) {
+  char string = NULL;
+  size_t size_string = 0;
+  // for regex
+  size_t nmatch = 1;
+  regmatch_t pmatch[1];
+  regex_t buffer;
+  int temp, regex_flag;
+
+  if (data -> mode.i == 0) {
+    regex_flag = REG_NEWLINE | REG_EXTENDED | REG_ICASE;
+  } else {
+    regex_flag = REG_NEWLINE | REG_EXTENDED;
+  }
+  if (regcomp(&buffer, data -> pattern.str_p, regex_flag) == 0) {
+
+  }
+  regfree(&buffer);
 }
 
 void read_file(char *string_file, int *sf, char *pattern,
@@ -276,6 +329,23 @@ void read_file(char *string_file, int *sf, char *pattern,
   regfree(&buffer);
   free(string);
 }
+
+
+// void open_file(char **files, int *sf, char *pattern, struct all_flags value) {
+//   FILE *f;
+//   for (int t = 0; t < *sf; t++) {
+//     if ((f = fopen(files[t], "r")) == NULL) {
+//       if (value.s == 0) {
+//         fprintf(stderr, "%s: no such file\n", files[t]);
+//       }
+//     } else {
+//       char *string_file = files[t];
+//       read_file(string_file, sf, pattern, value, f);  // string = files[t]
+//       fclose(f);
+//     }
+//   }
+// }
+
 
 void newstr(struct all_flags value, char *string_file, char *string, int n,
             int *sf, char *pattern, regex_t buffer) {
--- c_language/src/grep/grep.h
+++ c_language/src/grep/grep.h
@@ -1,1 +1,39 @@
+#ifndef UTILITES_C_LANGUAGE_GREP_GREP_H_
+#define UTILITES_C_LANGUAGE_GREP_GREP_H_
 
+
+struct GrepFlags {
+  int e;
+  int i;
+  int v;
+  int c;
+  int l;
+  int n;
+  int h;
+  int s;
+  int f;
+  int o;
+};
+
+struct GrepPattern {
+    // string pattern
+    char *str_p; // this is the pattern we will be looking for
+    size_t size_p;
+};
+
+struct GrepFiles {
+    // string files
+    char **str_f; // this is the pattern we will be looking for
+    size_t size_f;
+};
+
+struct GrepDates {
+    struct GrepFlags mode;
+    struct GrepPattern pattern; //the introduced pattern
+    struct GrepFiles files;
+    int last_position;  // reading position
+    int fail;
+};
+
+
+#endif // UTILITES_C_LANGUAGE_GREP_GREP_H_
